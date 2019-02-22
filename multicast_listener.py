c2c3b627364c6d245ce011d476d8ef4549e53f75
Odd Stråbø <oddstr13@openshell.no>
2019-02-23 00:14:38 +0100

    multicast_listener.py: Get list of interfaces (windows only for now)

--- multicast_listener.py
+++ multicast_listener.py
@@ -1,5 +1,8 @@
 import socket
 import struct
+import subprocess
+
+import arrow
 
 # https://bugs.python.org/issue29515
 IPPROTO_IPV6 = getattr(socket, 'IPPROTO_IPV6', 41)
@@ -11,6 +14,52 @@ MULTICAST_INTERFACE = 8
 # https://www.random.org/integers/?num=1&min=5001&max=49151&col=5&base=10&format=html&rnd=new
 # http://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.txt
 
+
+def cmd(args, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, encoding=None):
+    #print(args)
+    p = subprocess.Popen(args, stdout=stdout, stderr=stderr)
+    if p.stdout is not None:
+        if encoding is not None:
+            return (p.stdout.read()).decode(encoding)
+        return p.stdout.read()
+    else:
+        p.wait()
+        return p.returncode
+
+def windowsParseRoute():
+    data = cmd(["route", "print"], encoding="UTF8").replace('\r\n', '\n')
+    
+    res = {}
+    for block in data.split('\n\n'):
+        block = block.strip("=").strip()
+        if block.startswith("Interface List"):
+            res['interfaces'] = {}
+            _header, blockdata = block.split('\n', 1)
+            for line in blockdata.split('\n'):
+                line = line.strip()
+                ifid, rest = line.split('...', 1)
+                mac, rest = rest.split('.', 1)
+                name = rest.lstrip('.')
+                res['interfaces'][int(ifid)] = {
+                    'name': name.strip()
+                }
+                if mac:
+                    res['interfaces'][int(ifid)]['mac'] = ':'.join(mac.split())
+        # TODO: IPv4 & IPv6 route info parsing not implemented
+        #elif block.startswith("IPv4 Route Table"):
+        #    for _block in block.split("===="):
+        #        _block = _block.strip('=').strip()
+        #        if not _block: continue
+                #print("%%", _block)
+        #else:
+        #    pass
+            #print("###", block)
+
+    return res
+
+def getInterfaces():
+    # TODO: Make multi-platform
+    return list(windowsParseRoute().get('interfaces').keys())
 
 # Initialise socket for IPv6 datagrams
 sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -29,7 +78,7 @@ sock.bind(('::', MULTICAST_PORT))
 sock.setsockopt(IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, True)
 
 # Construct message for joining multicast group
-for ifnum in [1, 8, 13]: # On interfaces 1 (loopback), 8 (Ethernet) and 13 (vbox-hostonly) # TODO: Find a way to generate this list.
+for ifnum in getInterfaces():
     mreq = socket.inet_pton(socket.AF_INET6, MULTICAST_ADDRESS) + struct.pack('@L', ifnum) # Multicast address + interface
     sock.setsockopt(IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
 
@@ -39,6 +88,6 @@ sock.settimeout(5)
 while True:
     try:
         data, addr = sock.recvfrom(1500)
-        print(data, addr)
+        print(arrow.now(), data, addr)
     except socket.timeout:
         pass
